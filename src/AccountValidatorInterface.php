fe45b27d042ebbb9739e3336834b6562a19e4d99
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2020-10-15 20:21:53 -0400

    Added account validator stuff.

---
+++ src/AccountValidatorInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\drupalauth4ssp;
+
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Represents an interface for account validators.
+ *
+ * Account validators are used to ensure a user account is SSO-enabled.
+ */
+interface AccountValidatorInterface {
+
+  /**
+   * Checks to see if this account is SSO-enabled.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Account to check
+   * @return bool 'TRUE' if account is SSO-enabled; else 'FALSE'
+   */
+  public function isAccountValid(AccountInterface $account) : bool;
+
+}
---
+++ src/ChainedAccountValidator.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\drupalauth4ssp;
+
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Represents a chain of other account validators.
+ * 
+ * If any of the validators returns 'FALSE,' validation is refused. Else,
+ * validation is granted.
+ */
+class ChainedAccountValidator implements AccountValidatorInterface {
+
+  /**
+   * Array of sub-validators making up the validator chain.
+   *
+   * @var array
+   */
+  protected $subvalidators = [];
+
+  /**
+   * Adds a validator to the chain of validators.
+   *
+   * @param AccountValidatorInterface $validator
+   *   Validator to add
+   * @return void
+   */
+  public function addValidator(AccountValidatorInterface $validator) : void {
+    $this->subvalidators[] = $validator;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function isAccountValid(AccountInterface $account) : bool {
+    foreach ($this->subvalidators as $validator) {
+      if (!$validator->isAccountValid($account)) {
+        return FALSE;
+      }
+    }
+    return TRUE;
+  }
+
+}
