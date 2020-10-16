e098db136950728b1890541971881da1dfd8f46d
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2020-10-16 11:36:00 -0400

    Finished first SSO logout controller iteration.

--- src/Controller/SsoLogoutController.php
+++ src/Controller/SsoLogoutController.php
@@ -7,6 +7,9 @@ namespace Drupal\drupalauth4ssp\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
+use Drupal\drupalauth4ssp\Helper\ReturnToUrlManager;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Controller for handling logouts initiated by simpleSAMLphp.
@@ -28,16 +31,26 @@ class SsoLogoutController extends ControllerBase implements ContainerInjectionIn
   protected $requestStack;
 
   /**
+   * Helper service to obtain and determine if 'ReturnTo' URL can be used.
+   *
+   * @var \Drupal\drupalauth4ssp\Helper\ReturnToUrlManager;
+   */
+  protected $returnToUrlManager;
+
+  /**
    * Creates this controller object.
    *
    * @param \Drupal\Core\Session\AccountInterface $account
    *   Account.
    * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
    *   Request stack.
+   * @param \Drupal\drupalauth4ssp\Helper\ReturnToUrlManager $returnToUrlManager
+   *   Helper service to obtain and determine if 'ReturnTo' URL can be used
    */
-  public function __construct(AccountInterface $account, $requestStack) {
+  public function __construct(AccountInterface $account, $requestStack, $returnToUrlManager) {
     $this->account = $account;
     $this->requestStack = $requestStack;
+    $this->returnToUrlManager = $returnToUrlManager;
   }
   /**
    * Contains controller logic.
@@ -47,15 +60,27 @@ class SsoLogoutController extends ControllerBase implements ContainerInjectionIn
    *   or, if that URL doesn't exist, to the home page.
    */
   public function handle() {
-    $request = $requestStack->getCurrentRequest();
-    if (!$account->isAnonymous())
+    $request = $this->requestStack->getCurrentRequest();
+    if (!$this->account->isAnonymous()) {
       //Log the user out.
       user_logout();
     }
-    // Redirect, if possible (if the return URL parameter exists and is local
-    // and valid) to the return URL query string parameter. Otherwise, redirect
-    // to the home page.
-    $request = $request
+    // Attempt to redirect, if possible (if the return URL parameter isn't
+    // empty and is allowed) to the return URL query string parameter.
+    // Otherwise, redirect to the home page.
+    if ($this->returnToUrlManager->isReturnUrlValid()) {
+      return new RedirectResponse($this->returnToUrlManager->getReturnUrl());
+    }
+    else {
+      return new RedirectResponse(Url::fromRoute('<front>')->setAbsolute()->toString());
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('current_user'), $container->get('request_stack'), $container->get('drupalauth4ssp.return_url_manager'));
   }
 
 }
