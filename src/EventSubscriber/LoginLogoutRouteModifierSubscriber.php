aed09547973dbddef3a1770d8f80d0ef4be93923
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2020-11-13 16:26:53 -0500

    Added route modifier to make login/logout routes accessible.

---
+++ src/EventSubscriber/LoginLogoutRouteModifierSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\drupalauth4ssp\EventSubscriber;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Modifies login and logout routes appropriately.
+ *
+ * Modifies user.login and user.logout routes so that they are accessible to
+ * authenticated/unauthenticated users, respectively. For the reasons behind
+ * these changes, @see
+ * \Drupal\drupalauth4ssp\EventSubscriber\NormalLogoutLoginRouteRequestSubscriber.
+ */
+class LoginLogoutRouteModifierSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes($collection) {
+    // First, modify the user.login route.
+    $loginRoute = $collection->get('user.login');
+    if ($loginRoute) {
+      // If the route is defined, ensure anyone can access it.
+      $loginRoute->setRequirements(['_access' => 'TRUE']);
+    }
+    // Next, modify the user.logout route.
+    $logoutRoute = $collection->get('user.logout');
+    if ($logoutRoute) {
+      // If the route is defined, ensure anyone can access this route.
+      $logoutRoute->setRequirements(['_access' => 'TRUE']);
+    }
+  }
+
+}
