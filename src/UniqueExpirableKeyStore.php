3acb739ae994170aeb17a27db2d25944d90ae60e
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2020-12-02 21:03:14 -0500

    Added unique expirable key store class.

---
+++ src/UniqueExpirableKeyStore.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\drupalauth4ssp\UniqueExpirableKeyStore;
+
+/**
+ * Represents a unique storage mechanism that allows one to store in
+ * request-persistent storage a set of keys, all of which are unique within a
+ * given store. Optionally, each key may have an expiry time.
+ */
+class UniqueExpirableKeyStore {
+
+  public function __construct() {
+
+  }
+
+}
